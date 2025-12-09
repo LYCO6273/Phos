7a9713664f90e8e48094e67e2ebf9f53130124fc
LYCO6273 <lyco_6273>
2025-12-09 15:39:00 +0800

    Changes to be committed: deleted:    .devcontainer/devcontainer.json modified:   Phos_0.1.0.py

--- Phos_0.1.0.py
+++ Phos_0.1.0.py
@@ -1,3 +1,32 @@
+"""
+"No LUTs, we calculate LUX."
+
+你说的对，但是 Phos. 是基于「计算光学」概念的胶片模拟。
+通过计算光在底片上的行为，复现自然、柔美、立体的胶片质感。
+
+这是一个原理验证demo，图像处理部分基于opencv，交互基于
+streamlit平台制作，部分代码使用了AI辅助生成。
+
+如果您发现了项目中的问题，或是有更好的想法想要分享，还请
+通过邮箱 lyco_p@163.com 与我联系，我将不胜感激。
+
+Hello! Phos. is a film simulation app based on 
+the idea of "Computational optical imaging“. 
+By calculating the optical effects on the film,
+we could recurrent the natural, soft, and elegant
+tone of these classical films.
+
+This is a demo for idea testing. The image processing
+part is based on OpenCV, and the interaction is built
+on the Streamlit platform. Some of the code was
+generated with the assistance of AI.
+
+If you find any issues in the project or have better
+ideas you would like to share, please contact me via
+email at lyco_p@163.com. I would be very grateful.
+
+"""
+
 import streamlit as st
 
 # 设置页面配置 
